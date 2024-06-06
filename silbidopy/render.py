a81e96e9fa574342fca8323a02fa0ef814f5c51b
Joshua Zingale <jtzingale@gmail.com>
2024-06-06 10:23:11 -0700

    Added complex spectrogram function

--- silbidopy/render.py
+++ silbidopy/render.py
@@ -4,6 +4,104 @@ import wavio
 import math
 from math import ceil
 
+
+def getFrames(audioFile, frame_time_span = 8, step_time_span = 2,
+                   start_time = 0, end_time=-1, window_fn = None, get_sequence = False):
+    '''Frames a portion of the pressure measurements from an audio file.'''
+    
+    freq_resolution = 1000 / frame_time_span
+
+    # Load audio file
+    if type(audioFile) == wavio.Wav:
+        wav_data = audioFile
+    else:
+        wav_data = wavio.read(audioFile)
+    
+    # Rescale data if sample width is > 2
+    if wav_data.sampwidth > 2:
+            wav_data.data //= 2 ** (8 * (wav_data.sampwidth - 2))
+
+    # #
+    # Split the wave signal into overlapping frames
+    # #
+
+    start_frame = int(start_time / 1000 * wav_data.rate)
+    end_frame = int((end_time / 1000 + frame_time_span / 1000 - step_time_span / 1000)* wav_data.rate)
+
+    frame_sample_span = int(math.floor(frame_time_span / 1000 * wav_data.rate))
+    step_sample_span = step_time_span / 1000 * wav_data.rate
+    # No frames if the audio file is too short
+    if wav_data.data[start_frame:end_frame].shape[0] < frame_sample_span:
+        frames = []
+        return np.array(frames)
+    else:
+        frames = frame_signal(wav_data.data.ravel()[start_frame:end_frame], frame_sample_span, step_sample_span)
+    
+    if window_fn != None:
+       frames = frames * window_fn(frames.shape[1]) 
+    
+    if get_sequence:
+        return frames, wav_data.data.ravel()[start_frame:end_frame]
+    return frames
+
+def getComplexSpectrogram(audioFile, frame_time_span = 8, step_time_span = 2,
+        min_freq = 5000, max_freq = 50000,
+        start_time = 0, end_time=-1, window_fn = None):
+    '''
+    Gets and returns a two-dimensional list in which the values encode a spectrogram.
+
+    :param audioFile: the audio file in .wav format for which a spectrogram is generated.
+        This may either be an audio file of type wavio.Wav or a file name
+    :param frame_time_span: ms, length of time for one time window for dft
+    :param step_time_span: ms, length of time step for spectrogram
+    :param spec_clip_min: log magnitude spectrogram min-max normalization, minimum value
+    :param spec_clip_max: log magnitude spectrogram min-max normalization, maximum value
+    :param min_freq: Hz, lower bound of frequency for spectrogram
+    :param max_freq: Hz, upper bound of frequency for spectrogram
+    :param start_time: ms, the beginning of where the audioFile is read
+    :param end_time: ms, the end of where the audioFile is read. If end > the length of
+        of the file, then the file is read only to its end.
+    :param window_fn: the function that generates a processing map for each frame
+        before the the frames are used in the spectrogram. The function must receive
+        one positional argument, n, and then return an array of length n.
+        For example, window_fn(5) could return [0.1,0.2,0.4,0.2,0.1]
+    :returns: A tuple with both the spectrogram and the time at which the
+        spectrogram ended in ms: (spectogram, end_time)
+    '''
+
+    freq_resolution = 1000 / frame_time_span
+
+    frames = getFrames(audioFile,
+            frame_time_span = frame_time_span,
+            step_time_span = step_time_span,
+            start_time = start_time,
+            end_time = end_time, window_fn = window_fn)
+
+    # No spectrogram if the audio file is too short
+    if len(frames) == 0:
+        return np.array([[]], dtype=float), start_time
+    
+    # #
+    # Make spectrogram
+    # #
+    NFFT = len(frames[0])
+
+    # Compute magnitude spectra
+    singal_magspec = np.fft.rfft(frames, NFFT)
+
+    # Include only the desired frequency range
+    clip_bottom = int(min_freq // freq_resolution)
+    clip_top = int(max_freq // freq_resolution) 
+    spectrogram = singal_magspec.T[clip_bottom:clip_top]
+
+    # Flip spectrogram to match expectations for display
+    # Also normalize
+    # spectrogram = spectrogram[::-1,]
+
+    actual_end_time = start_time + spectrogram.shape[1] * step_time_span
+    return spectrogram, actual_end_time
+
+
 def getSpectrogram(audioFile, frame_time_span = 8, step_time_span = 2, spec_clip_min = 0,
                    spec_clip_max = 6, min_freq = 5000, max_freq = 50000,
                    start_time = 0, end_time=-1, window_fn = None):
@@ -76,9 +174,8 @@ def getSpectrogram(audioFile, frame_time_span = 8, step_time_span = 2, spec_clip
 
     # Flip spectrogram to match expectations for display
     # Also normalize
-    spectrogram = normalize3(spectrogram[::-1,], spec_clip_min, spec_clip_max)
-
-
+    spectrogram = spectrogram[::-1,]
+    spectrogram = normalize3(spectrogram, spec_clip_min, spec_clip_max)
 
     actual_end_time = start_time + spectrogram.shape[1] * step_time_span
     return spectrogram, actual_end_time
